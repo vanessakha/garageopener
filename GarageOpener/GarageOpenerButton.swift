0181bfd6cbd5affad4bd8fd9e137af0ae9ae1129
Vanessa Kha <vanessakha@Vanessas-MacBook-Pro.local>
2017-10-18 00:32:50 -0700

    first commit

---
+++ GarageOpener/GarageOpenerButton.swift
@@ -0,0 +1,58 @@
+//
+//  GarageOpenerButton.swift
+//  GarageOpener
+//
+//  Created by Vanessa on 10/15/17.
+//  Copyright © 2017 Vanessa. All rights reserved.
+//
+
+import Foundation
+
+class Webiopi {
+    //    let webiopiServerAddress: String = "192.168.1.10"
+    //    let webiopiServerPort: Int = 8000
+    let webiopiServerAddress: String = "cef71d2a.ngrok.io"
+    let webiopiServerPort: Int = 80
+    
+    func constructURLRequest(url: String) -> URLRequest {
+        var request = URLRequest(url: NSURL(string: url)! as URL)
+        request.httpMethod = "POST"
+        
+        // Set the login user/password as the HTTP Authorization header
+        // Authorization: Basic <base64-encoded-user-colon-password>
+        // where <base64-encoded-user-colon-password> = Base64-Encode of (user ":" password)
+        let username = "webiopi"
+        let password = "raspberry"
+        let loginString = String(format: "%@:%@", username, password)
+        let loginData = loginString.data(using: String.Encoding.utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    //    func prepareHttpRequestToSetGpioPin(gpioNumber: Int, on: Bool) throws -> URLRequest {
+    //        let gpioValue: Int = on ? 1 : 0
+    //        let url = String(format: "http://%@:%d/GPIO/%d/value/%d",
+    //                         webiopiServerAddress, webiopiServerPort, gpioNumber, gpioValue)
+    //        return constructURLRequest(url: url)
+    //    }
+    //
+    func prepareHttpRequestToSendPulse(gpio gpioNumber: Int) throws -> URLRequest {
+        let url = String(format: "http://%@:%d/GPIO/%d/sequence/1000,10",
+                         webiopiServerAddress, webiopiServerPort, gpioNumber)
+        return constructURLRequest(url: url)
+    }
+    
+    func sendPulse(gpioNumber: Int) throws {
+        // Perform the request to send a sequence to the gpio pin gpioNumber to simulate a long pulse
+        let requestOn = try prepareHttpRequestToSendPulse(gpio: gpioNumber)
+        let response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
+        let data = try NSURLConnection.sendSynchronousRequest(requestOn, returning: response)
+        print(data)
+    }
+    
+}
+
+
+
+
--- GarageOpener/ViewController.swift
+++ GarageOpener/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func openGarage(_ sender: Any) {
+        let webiopi = Webiopi()
+        do{
+            try webiopi.sendPulse(gpioNumber: 4)
+        }catch{
+            //TODO: pop-up UI for error
+        }
     }
-
-
+    
 }
 
